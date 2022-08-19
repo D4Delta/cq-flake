8fc6d403e7b96092be60d78ce3c09c6b23f9425a
Delta <d4delta@outlook.fr>
2022-08-19 12:03:35 +0200

    shit works

--- expressions/py-overrides.nix
+++ expressions/py-overrides.nix
@@ -137,4 +137,16 @@
       sha256 = "611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7";
     };
   });
+
+
+  eventlet = super.eventlet.overridePythonAttrs (oldAttrs: {
+    doCheck = false;
+    doInstallCheck = false;
+  });
+  
+  uvloop = super.uvloop.overridePythonAttrs (oldAttrs: {
+    doCheck = false;
+    doInstallCheck = false;
+  });
+
 }
